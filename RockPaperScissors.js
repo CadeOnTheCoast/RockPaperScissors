4e2f9e844c88c2944d6e2f2cddce9cf28f7dd2b6
CadeOnTheCoast <cade.kistler@gmail.com>
2020-09-01 09:02:09 -0500

    fixed the scoring mechanism and some variable scoping issues.

--- RockPaperScissors.js
+++ RockPaperScissors.js
@@ -1,5 +1,6 @@
 let playerScore = 0;
 let computerScore = 0;
+let totalScore = 0;
 let playerSelection;
 let computerSelection;
 function computerPlay() {
@@ -13,36 +14,40 @@ function playRound(playerSelection, computerSelection) {
             alert("Tie, play again.");
         } else if (playerSelection.toLowerCase() == "rock" && computerSelection == "paper") {
             alert("Paper covers rock, you lose.");
-            computerScore ++;
+            computerScore += 1;
+            totalScore += 1;
         } else if (playerSelection.toLowerCase() == "rock" && computerSelection == "scissors") {
             alert("Rock destroys scissors, you win!");
-            playerScore ++;
+            playerScore += 1;
+            totalScore += 1;
         } else if (playerSelection.toLowerCase() == "paper" && computerSelection == "scissors") {
             alert("Scissors cut paper, you lose.");
-            computerScore ++;
+            computerScore += 1;
+            totalScore += 1;
         } else if (playerSelection.toLowerCase() == "paper" && computerSelection == "rock") {
             alert("Paper covers rock, you win!");
-            playerScore ++;
+            playerScore += 1;
+            totalScore += 1;
         } else if (playerSelection.toLowerCase() == "scissors" && computerSelection == "paper") {
             alert("Scissors cut paper you win!");
-            playerScore ++;
+            playerScore += 1;
+            totalScore += 1;
         } else if (playerSelection.toLowerCase() == "scissors" && computerSelection == "rock") {
             alert("Rock smashes scissors, you lose.");
-            computerScore ++;
+            computerScore += 1;
+            totalScore += 1;
         } else {
             return "??";
         }
 }
 
 function game(){
-    for (let i = 0; i < 5; i++) {
+    while (totalScore <= 5) {
         playerSelection = prompt("Please choose paper, rock, or scissors!)");
         computerSelection = computerPlay();
-        if (playerSelection == computerSelection){
-            i --;
-            alert("Tie, play again.");}
         playRound(playerSelection, computerSelection); 
+        alert("The score is" + playerScore + "computer" + computerScore+ "total" + totalScore);
     }
-    alert ("The final score was, You: " + Math.abs(playerScore) + " Computer: " + Math.abs(computerScore));
+    alert ("The final score was, You: " + playerScore + " Computer: " + computerScore);
 }
 game();
